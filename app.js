6bc4b7e55881eb640987d9cdf710c33770bacf2a
Burbinox <54470991+Burbinox@users.noreply.github.com>
2022-02-17 23:06:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log("Connected to db"))
 
-// allow express get json in post body
+// allow express get json in request body
 app.use(express.json())
 
 const exampleModule = require('./urls/example')
