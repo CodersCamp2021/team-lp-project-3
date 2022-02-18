0300940164cb5c787d72f53c0a8a17b3240d6306
patrykbrodziak <patryk.brodziak1@gmail.com>
2022-02-18 16:45:01 +0100

    create cloud database

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose') 
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config();
 
 // DB setup 
-mongoose.connect('mongodb://localhost/gameweb')
+mongoose.connect(process.env.DATABASE_PASSWORD, {useNewUrlParser:true})
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log("Connected to db"))
