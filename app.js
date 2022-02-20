ab70500faa182cc94ca885ed3594fba4093d542b
Jakub Czerwinski <kubaczerwinski77@gmail.com>
2022-02-20 13:56:22 +0100

    changed names

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-import exampleModule from './routes/example.js';
+import exampleRouter from './routes/example.js';
 
 const app = express();
 dotenv.config();
@@ -16,7 +16,7 @@ db.once('open', () => console.log('Connected to database.'));
 // allow express get json in request body
 app.use(express.json());
 
-app.use('/example', exampleModule);
+app.use('/example', exampleRouter);
 
 app.listen(3000, () =>
   console.log('Server running on http://localhost:3000...'),
