7a8bfc5ccac98798e62c7f52668958c9df216085
patrykbrodziak <patryk.brodziak1@gmail.com>
2022-02-23 18:45:18 +0100

    spaces/tabs fix

--- models/gameModel.js
+++ models/gameModel.js
@@ -1,33 +1,33 @@
 import mongoose from "mongoose";
 
 const gameSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    category: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true,
-        maxLength: 1000
-    },
-    platform: {
-        type: String,
-        required: true,
-        enum: ['PS4', 'PS5', 'PC', 'XBOX ONE', 'XBOX SERIES S/X']
-    },
-    developer: {
-        type: String,
-        required: true
-    },
-    release_date: {
-        type: Date,
-        required: true
-    },
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true,
+    maxLength: 1000
+  },
+  platform: {
+    type: String,
+    required: true,
+    enum: ['PS4', 'PS5', 'PC', 'XBOX ONE', 'XBOX SERIES S/X']
+  },
+  developer: {
+    type: String,
+    required: true
+  },
+  release_date: {
+    type: Date,
+    required: true
+  },
 })
 
 export const Game = mongoose.model('Game', gameSchema)
