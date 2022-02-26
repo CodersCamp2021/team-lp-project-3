453d0741c7835353779d5697c8147019bdbb5e8b
pastepi <pawel.st123@gmail.com>
2022-02-26 21:27:29 +0100

    Added basic DELETE endpoint

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -9,6 +9,18 @@ class GameController {
       res.status(404).json({ message: error });
     }
   };
+
+  deleteGame = async (req, res) => {
+    try {
+      const game = await Game.findByIdAndDelete(req.params.gameId);
+      res.status(200).json({
+        _id: game._id,
+        message: `${game.title} has been successfully deleted from the DB.`,
+      });
+    } catch (error) {
+      res.status(404).json({ message: 'Game not found.' });
+    }
+  };
 }
 
 export default new GameController();
--- routes/game.js
+++ routes/game.js
@@ -4,5 +4,6 @@ import GameController from '../controllers/gameController.js';
 const router = express.Router();
 
 router.get('/:gameId', GameController.getGameDetails);
+router.delete('/:gameId', GameController.deleteGame);
 
 export { router as gameRouter };
