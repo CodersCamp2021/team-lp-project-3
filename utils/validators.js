ff5a42428ad21b50ee656d4a2d1d8ebe04ea2593
pastepi <pawel.st123@gmail.com>
2022-02-26 16:41:59 +0100

    Formatting/spacing fix

--- utils/validators.js
+++ utils/validators.js
@@ -4,33 +4,33 @@ import { Game } from '../models/gameModel.js';
 
 export function validateRequest(req, res, next) {
   const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-  
-    next();
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
   }
+
+  next();
+}
 
 const gameValidator = [
   body('title').custom((value) => {
     return Game.findOne({ title: value }).then((game) => {
       if (game) {
         return Promise.reject('Game title already exist');
-        }
-      });
-    }),
+      }
+    });
+  }),
   body('category').exists().isString(),
   body('description').exists().isLength({ max: 1000 }),
   body('platform').custom((value) => {
     var platforms = ['PS4', 'PS5', 'PC', 'XBOX ONE', 'XBOX SERIES S/X'];
-    return Game.findOne({ platform: value }).then(() =>{
-      if(!platforms.includes(value)){
+    return Game.findOne({ platform: value }).then(() => {
+      if (!platforms.includes(value)) {
         return Promise.reject('Platform not exist');
       }
-    })  
+    });
   }),
   body('developer').exists().isString(),
   body('releaseDate').exists().isDate(),
-]
+];
 
-export {gameValidator};
+export { gameValidator };
