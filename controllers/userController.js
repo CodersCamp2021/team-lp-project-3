496a26860ae60e3edc1e0678e48c96b6b2d03dec
pastepi <pawel.st123@gmail.com>
2022-02-28 21:55:37 +0100

    Method name change

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 import { User } from '../models/userModel.js';
 
 class UserController {
-  updateUser = async () => {};
+  changeUserCredentials = async () => {};
 }
 
 export default UserController;
