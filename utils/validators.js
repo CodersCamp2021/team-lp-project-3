6b15b39834f2c48d93a8995d03008edcab10ed9d
Maciek <maciek.duzy.11@gmail.com>
2022-03-01 22:09:16 +0100

    add login validation

--- utils/validators.js
+++ utils/validators.js
@@ -56,4 +56,16 @@ const registerValidator = [
   body('password').isLength({ min: 6, max: 1024 }),
 ];
 
-export { registerValidator, gameValidator };
+const loginValidator = [
+  body('email').isEmail().isLength({ min: 6, max: 255 }),
+  body('email').custom((value) => {
+    return User.findOne({ email: value }).then((user) => {
+      if (!user) {
+        return Promise.reject('E-mail not found.');
+      }
+    });
+  }),
+  body('password').isLength({ min: 6, max: 1024 }),
+];
+
+export { registerValidator, loginValidator, gameValidator };
