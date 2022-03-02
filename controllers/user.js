3e71754ab5e7667f4f0393395df0de8668936c09
Burbinox <hubert.grobelny@interia.pl>
2022-03-02 22:51:42 +0100

    add expiry date

--- controllers/user.js
+++ controllers/user.js
@@ -64,7 +64,9 @@ export default class UserController {
       return res.status(400).send('Invalid password');
     }
 
-    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET, {
+      expiresIn: '30m',
+    });
     res.header('auth-token', token).send(token);
   };
 }
