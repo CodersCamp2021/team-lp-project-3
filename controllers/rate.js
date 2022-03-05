614d2a172037f9716741157ae63d93552b56a904
pastepi <pawel.st123@gmail.com>
2022-03-05 23:50:07 +0100

    Removed comment

--- controllers/rate.js
+++ controllers/rate.js
@@ -14,14 +14,6 @@ class RateController {
       const rate = await Rate.findOne({ gameId: game._id, userId: user._id });
       if (!rate) return res.status(200).json({ rating: 0 });
       return res.status(200).json({ rating: rate.rating });
-
-      // // check if game was rated before
-      // const rateExists = await User.findOne({ rating: req.body.rating });
-      // if (rating !== 0) {
-      //   return res.status(400).json({ message: 'Rate has been given.' });
-      // }
-
-      // user.rateExist = req.body.rating;
     } catch (error) {
       res.status(500).json({ message: error });
     }
