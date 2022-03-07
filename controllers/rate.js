cef7264ae199af9ca25e8306ea691c76474fc9ea
pastepi <pawel.st123@gmail.com>
2022-03-07 01:27:16 +0100

    Refactored rate router/controller in prep for tests

--- controllers/rate.js
+++ controllers/rate.js
@@ -3,79 +3,83 @@ import { User } from '../models/user.js';
 import { Rate } from '../models/rate.js';
 
 class RateController {
-  getGameRate = async (req, res) => {
-    try {
-      const game = await Game.findById(req.body.gameId);
-      if (!game) return res.status(404).json({ error: 'Game not found.' });
-
-      const user = await User.findById(req.body.userId);
-      if (!user) return res.status(404).json({ error: 'User not found.' });
-
-      const rate = await Rate.findOne({ gameId: game._id, userId: user._id });
-      if (!rate) return res.status(200).json({ rating: 0 });
-      return res.status(200).json({ rating: rate.rating });
-    } catch (error) {
-      res.status(500).json({ message: error });
+  getGameRate = async (body) => {
+    const { gameId, userId } = body;
+    const game = await Game.findById(gameId);
+    if (!game) {
+      throw new Error('Game not found.');
     }
+
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new Error('User not found.');
+    }
+
+    const rate = await Rate.findOne({ gameId: game._id, userId: user._id });
+    if (!rate) {
+      return { rating: 0 };
+    }
+
+    return { rating: rate.rating };
   };
 
-  putGameRate = async (req, res) => {
-    try {
-      const game = await Game.findById(req.body.gameId);
-      if (!game) return res.status(404).json({ error: 'Game not found.' });
-
-      const user = await User.findById(req.body.userId);
-      if (!user) return res.status(404).json({ error: 'User not found.' });
-
-      // if rating === 0
-      if (!req.body.rating) {
-        // remove it from the collection
-        await Rate.findOneAndDelete({
-          gameId: req.body.gameId,
-          userId: req.body.userId,
-        });
-        // remove 'userId' from 'ratedBy'
-        if (game.ratedBy.includes(req.body.userId)) {
-          game.ratedBy.splice(game.ratedBy.indexOf(req.body.userId), 1);
-        }
-        // remove 'gameId' from 'ratedGames'
-        if (user.ratedGames.includes(req.body.gameId)) {
-          user.ratedGames.splice(user.ratedGames.indexOf(req.body.gameId), 1);
-        }
-      } else {
-        // if rating !== 0
-        // add/update it in the collection
-        await Rate.findOneAndUpdate(
-          {
-            gameId: req.body.gameId,
-            userId: req.body.userId,
-          },
-          {
-            rating: req.body.rating,
-          },
-          {
-            upsert: true,
-            new: true,
-          },
-        );
-        // push 'userId' to 'ratedBy'
-        if (!game.ratedBy.includes(req.body.userId)) {
-          game.ratedBy.push(req.body.userId);
-        }
-        // push 'gameId' to 'ratedGames'
-        if (!user.ratedGames.includes(req.body.gameId)) {
-          user.ratedGames.push(req.body.gameId);
-        }
-      }
-
-      await game.save();
-      await user.save();
-
-      return res.status(200).json({ message: 'Rating updated.' });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ error });
+  putGameRate = async (body) => {
+    const { gameId, userId, rating } = body;
+    const game = await Game.findById(gameId);
+    if (!game) {
+      throw new Error('Game not found.');
     }
+
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new Error('User not found.');
+    }
+
+    // if rating === 0
+    if (!rating) {
+      // remove it from the collection
+      await Rate.findOneAndDelete({
+        gameId: gameId,
+        userId: userId,
+      });
+      // remove 'userId' from 'ratedBy'
+      if (game.ratedBy.includes(userId)) {
+        game.ratedBy.splice(game.ratedBy.indexOf(userId), 1);
+      }
+      // remove 'gameId' from 'ratedGames'
+      if (user.ratedGames.includes(gameId)) {
+        user.ratedGames.splice(user.ratedGames.indexOf(gameId), 1);
+      }
+    } else {
+      // if rating !== 0
+      // add/update it in the collection
+      await Rate.findOneAndUpdate(
+        {
+          gameId: gameId,
+          userId: userId,
+        },
+        {
+          rating: rating,
+        },
+        {
+          upsert: true,
+          new: true,
+        },
+      );
+      // push 'userId' to 'ratedBy'
+      if (!game.ratedBy.includes(userId)) {
+        game.ratedBy.push(userId);
+      }
+      // push 'gameId' to 'ratedGames'
+      if (!user.ratedGames.includes(gameId)) {
+        user.ratedGames.push(gameId);
+      }
+    }
+
+    await game.save();
+    await user.save();
+
+    return { message: 'Rating updated.' };
   };
 }
 
--- routes/rate.js
+++ routes/rate.js
@@ -3,7 +3,23 @@ import RateController from '../controllers/rate.js';
 
 const router = express.Router();
 
-router.get('/', RateController.getGameRate);
-router.put('/', RateController.putGameRate);
+router.get('/', async (req, res) => {
+  try {
+    const rating = await RateController.getGameRate(req.body);
+    return res.status(200).json(rating);
+  } catch (error) {
+    return res.status(404).json({
+      error: error.message,
+    });
+  }
+});
+router.put('/', async (req, res) => {
+  try {
+    const message = await RateController.putGameRate(req.body);
+    return res.status(200).json(message);
+  } catch (error) {
+    return res.status(404).json({ error: error.message });
+  }
+});
 
 export { router as rateRouter };
